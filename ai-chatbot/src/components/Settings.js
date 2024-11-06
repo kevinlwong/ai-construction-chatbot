bb281f9b9b5f7c7fe790cdf633585f618ca85ad8
Kevin Wong <kevinwong973@gmail.com>
2024-11-06 11:34:41 -0800

    settings changes

--- ai-chatbot/src/components/Settings.js
+++ ai-chatbot/src/components/Settings.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useCallback, useContext, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
+import ThemeContext from "../context/ThemeContext.js"
+import './Settings.css'
 
 const SettingsContainer = styled.div`
   position: absolute;
@@ -9,9 +11,45 @@ const SettingsContainer = styled.div`
 `;
 
 const Settings = ({ toggleTheme }) => {
+  const [isSettingsOpen, setIsSettingsOpen] = useState(false);
+  const menuRef = useRef(null);
+  const buttonRef = useRef(null);
+  const { theme } = useContext(ThemeContext);
+
+  const toggleMenu = (event) => {
+    setIsSettingsOpen((prevState) => !prevState);
+    event.stopPropagation();
+  };
+
+  const handleClickOutside = useCallback((event) => {
+    if (
+      menuRef.current &&
+      !menuRef.current.contains(event.target) &&
+      buttonRef.current &&
+      !buttonRef.current.contains(event.target)
+    ) {
+      setIsSettingsOpen(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [handleClickOutside]);
+
   return (
     <SettingsContainer>
-      <button onClick={toggleTheme}>⚙️</button>
+      <button onClick={toggleMenu}>⚙️</button>
+
+      {isSettingsOpen && (
+        <div ref={menuRef} className={`settings-menu ${theme}`}>
+          <button onClick={toggleTheme} className="settings-option">Toggle Theme</button>
+          <div className="settings-option">User Settings</div>
+        </div>
+      )}
     </SettingsContainer>
   );
 };
